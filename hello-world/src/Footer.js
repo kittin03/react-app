b6462b08b37de4eb374cbd70b01ba43ffc8eaee5
kittin03 <katarinam@centrum.cz>
2016-08-11 02:20:29 +0200

    added component

---
+++ hello-world/src/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+
+        <p>
+          bla blaa bla
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
